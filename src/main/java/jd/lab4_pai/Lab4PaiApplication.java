7560a922ab1d0373778a4402952bfdb25dec4b7a
magiczny <magicznyjasio@gmail.com>
2023-04-09 17:59:07 +0200

    Initial commit

---
+++ src/main/java/jd/lab4_pai/Lab4PaiApplication.java
@@ -0,0 +1,13 @@
+package jd.lab4_pai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4PaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4PaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jd/lab4_pai/Lab4PaiApplicationTests.java
@@ -0,0 +1,13 @@
+package jd.lab4_pai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4PaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
